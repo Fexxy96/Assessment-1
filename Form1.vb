85f4dd165e8f05bcbd7c41413b79c78e995c2006
Dell-User <Dell-User@DESKTOP-72VMP8H>
2024-01-19 16:50:20 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Public Class Form1
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(TextBox1.Text.Trim)
+        Label2.Text = TextBox1.Text.Trim
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim word As String = ListBox1.Text
+        ListBox1.Items.Add(TextBox1.Text.Trim) = True Then
+            MessageBox.Show(" item already exist") 'duplication not allowed
+
+
+    End Sub
+End Class
